10a89c44298d8fb3bdf4e1e0e691fdaa5a5805d5
Thu Than <than@uni.minerva.edu>
2026-02-22 18:25:16 -0800

    added antialisasing

--- main.cpp
+++ main.cpp
@@ -1,73 +1,21 @@
-#include "color.h"
-#include "vec3.h"
-#include "ray.h"
+#include "rtweekend.h"
+#include "camera.h"
+#include "hittable.h"
+#include "hittable_list.h"
+#include "sphere.h"
 #include <iostream>
-
-double hit_sphere(const point3 &center, double radius, const ray &r)
-{
-    vec3 oc = center - r.origin();
-    auto a = dot(r.direction(), r.direction());
-    auto b = -2.0 * dot(r.direction(), oc);
-    auto c = dot(oc, oc) - radius * radius;
-    auto discriminant = b * b - 4 * a * c;
-
-    if (discriminant < 0)
-    {
-        return -1.0;
-    }
-    else
-    {
-        return (-b - std::sqrt(discriminant)) / (2.0 * a);
-    }
-}
-
-color ray_color(const ray &r)
-{
-    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
-    if (t > 0.0)
-    {
-        vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
-        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
-    }
-    vec3 unit_direction = unit_vector(r.direction());
-    auto a = 0.5 * (unit_direction.y() + 1.0);
-    return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
-}
 
 int main()
 {
-    auto aspect_ratio = 16.0 / 9.0;
-    int width = 400;
-    int height = (width / aspect_ratio < 1) ? 1 : int(width / aspect_ratio);
+    hittable_list world;
 
-    auto focal_length = 1.0;
-    auto viewport_height = 2.0;
-    auto viewport_width = viewport_height * (double(width) / height);
-    auto camera_center = point3(0, 0, 0);
+    world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
-    auto viewport_u = vec3(viewport_width, 0, 0);
-    auto viewport_v = vec3(0, -viewport_height, 0);
+    camera cam;
 
-    auto pixel_delta_u = viewport_u / width;
-    auto pixel_delta_v = viewport_v / height;
-
-    auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
-    auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
-
-    std::cout << "P3\n"
-              << width << ' ' << height << "\n255\n";
-
-    for (int i = 0; i < height; i++)
-    {
-        std::clog << "\rScanlines remaining: " << (height - i) << ' ' << std::flush;
-        for (int j = 0; j < width; j++)
-        {
-            auto pixel_center = pixel00_loc + (j * pixel_delta_u) + (i * pixel_delta_v);
-            auto ray_direction = pixel_center - camera_center;
-            ray r(camera_center, ray_direction);
-            color pixel_color = ray_color(r);
-            write_color(std::cout, pixel_color);
-        }
-    }
-    std::clog << "\rDone.               \n";
+    cam.aspect_ratio = 16.0 / 9.0;
+    cam.image_width = 400;
+    cam.samples_per_pixel = 100;
+    cam.render(world);
 }
---
+++ rtweekend.h
@@ -0,0 +1,41 @@
+#ifndef RTWEEKEND_H
+#define RTWEEKEND_H
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
+#include <limits>
+#include <memory>
+#include <random>
+
+using std::make_shared;
+using std::shared_ptr;
+
+// Constants
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = 3.1415926535897932385;
+
+// Utility
+inline double degrees_to_radians(double degrees)
+{
+    return degrees * pi / 180.0;
+}
+
+inline double random_double()
+{
+    // Returns a random real in [0,1).
+    return std::rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max)
+{
+    // Returns a random real in [min,max).
+    return min + (max - min) * random_double();
+}
+
+// Common headers
+#include "color.h"
+#include "ray.h"
+#include "vec3.h"
+#include "interval.h"
+
+#endif
